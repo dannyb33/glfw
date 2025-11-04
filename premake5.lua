1ecc889183fa5bbc58bec6c758618cf242c0eccb
dannyb33 <dannyb3@vt.edu>
2025-11-04 18:21:30 -0500

    premake update

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    architecture "x64"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
